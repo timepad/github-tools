2561112ca6d7072f237cd15d4d458cd5a9ef910d
artyfarty <mail@artyfarty.ru>
2017-05-17 01:38:33 +0300

    added date timezone

--- make.php
+++ make.php
@@ -3,21 +3,16 @@
  * @author artyfarty
  */
 
-use TpReleaseNotes\LocalGit\LocalGit;
+date_default_timezone_set("Europe/Moscow");
 
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
 
 $console = new Application();
 
 $console->add(new \TpReleaseNotes\Command\GenerateReleaseNotes('make_notes'));
 $console->add(new \TpReleaseNotes\Command\Stats('make_stats'));
-//$console->add(new \TpReleaseNotes\Command\RebindTags('rebind_tags'));
-//$console->add(new \TpReleaseNotes\Command\DestroyTags('destroy_tags'));
 
 $console->run();
 
